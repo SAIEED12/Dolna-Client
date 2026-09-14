e9b08d44167f887cf41926cd3a3ce680407acac4
SAIEED12 <abusaieed0@gmail.com>
2026-09-14 17:43:55 +0600

    Refactor Home component layout and styling

--- app/page.js
+++ app/page.js
@@ -2,10 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="">
-      Home Page
-      <br></br>
-      14-09-26
-    </div>
+<div className="bg-[#F5F1E8] flex min-h-screen flex-col">
+  Home Page
+  <br></br>
+  14-09-26
+</div>
   );
 }
